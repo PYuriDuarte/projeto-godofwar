a6b9c64627a836d23f97712176d3fb1c6e0ac4f5
Yuri Duarte <116809586+PYuriDuarte@users.noreply.github.com>
2022-11-25 08:37:32 -0300

    commit

--- js/main.js
+++ js/main.js
@@ -1,5 +1,28 @@
 var swiper = new Swiper(".slide-characteres", {
-  slidesPerView: 4,
+  slidesPerView: 3.3,
   spaceBetween: 10,
   freeMode: true,
+  breakpoints: {
+    320:{
+      slidesPerView: 1.2,      
+    },
+    768:{
+      slidesPerView: 2.02,      
+    },
+    991:{
+      slidesPerView: 2.7,      
+    },
+    1200:{
+      slidesPerView: 3.3,      
+    },
+  }
 });
+
+AOS.init({
+  duration: 1000,
+  once: true,
+});
+
+// var logo = document.querySelector(".logo");
+// logo.setAttribute("data-aos", "zoom-in");
+
